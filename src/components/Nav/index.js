747f2317b04ab40748eb88f0450166ac67fd94f2
ShepLT1 <lshepherd234@gmail.com>
2020-12-05 17:53:45 -0800

    Added social/resume link popups

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { Grid, Image, Icon } from 'semantic-ui-react';
+import { Grid, Image, Icon, Popup } from 'semantic-ui-react';
 import CoverPhoto from '../../images/Zoom-cover-photo.png';
 import Resume from '../../images/Luke-Shepherd-Resume.pdf';
 import "./style.css";
 
 function Nav() {
+
+  const popupStyle = {
+    opacity: .5,
+    height: 27.5,
+    padding: 5
+  }
 
   return (
     <Grid id="nav-container">
@@ -28,9 +34,33 @@ function Nav() {
             <a href="#contact" className="nav-item">Contact</a>
           </div>
           <div id="links-container">
-            <a href="https://github.com/ShepLT1" target="_blank" rel="noopener noreferrer" className="link"><Icon name='github' size="big" /></a>
-            <a href="https://www.linkedin.com/in/luke-t-shepherd" target="_blank" rel="noopener noreferrer" className="link"><Icon name='linkedin' size="big" /></a>
-            <a href={Resume} target="_blank" rel="noopener noreferrer" className="link"><Icon name='file alternate outline' size="big" /></a>
+            <Popup
+              trigger={<a href="https://github.com/ShepLT1" target="_blank" rel="noopener noreferrer" className="link"><Icon name='github' size="big" /></a>}
+              content='Github Profile'
+              size='tiny'
+              position="top right"
+              offset={[0, 7.5]}
+              style={popupStyle}
+              inverted
+            />
+            <Popup
+              trigger={<a href="https://www.linkedin.com/in/luke-t-shepherd" target="_blank" rel="noopener noreferrer" className="link"><Icon name='linkedin' size="big" /></a>}
+              content='LinkedIn Profile'
+              size='tiny'
+              position="top center"
+              offset={[0, 7.5]}
+              style={popupStyle}
+              inverted
+            />
+            <Popup
+              trigger={<a href={Resume} target="_blank" rel="noopener noreferrer" className="link"><Icon name='file alternate outline' size="big" /></a>}
+              content='Resume'
+              size='tiny'
+              position="top left"
+              offset={[0, 7.5]}
+              style={popupStyle}
+              inverted
+            />
           </div>
           <p id="copyright">&copy; 2020 Luke Shepherd</p>
         </Grid.Column>
