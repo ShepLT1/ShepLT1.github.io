82880d52ed820d68bb349ba134748991b8781542
ShepLT1 <lshepherd234@gmail.com>
2020-12-06 01:59:30 -0800

    narrowed divisions between expertise and project sections

--- src/components/Expertise/index.js
+++ src/components/Expertise/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid } from 'semantic-ui-react';
 import Heading from '../../components/Header';
 import ExpertSection from '../../components/ExpertSection';
+import './styles.css';
 
 function Expertise() {
 
@@ -10,19 +11,19 @@ function Expertise() {
       <Grid.Row>
         <Heading header="Expertise" />
       </Grid.Row>
-      <Grid.Row columns={2}>
-        <Grid.Column>
+      <Grid.Row columns={2} className="expert-row">
+        <Grid.Column className="expert-col">
           <ExpertSection icon="desktop" title="Front End" details="React -- React Hooks -- React Router -- Semantic UI -- HTML -- CSS -- JQuery -- Bootstrap" />
         </Grid.Column>
-        <Grid.Column>
+        <Grid.Column className="expert-col">
           <ExpertSection icon="server" iconClass="server-align" title="Back End" details="MySQL -- MongoDB -- Node -- Express -- Sequelize -- Mongoose -- AJAX -- JSON -- Web Services API" />
         </Grid.Column>
       </Grid.Row>
-      <Grid.Row columns={2}>
-        <Grid.Column>
+      <Grid.Row columns={2} className="expert-row">
+        <Grid.Column className="expert-col">
           <ExpertSection icon="cog" iconClass="cog-align" title="Other Tools" details="OOP -- MVC Paradigm -- Git -- GitHub -- Heroku -- TDD -- Computer Science" />
         </Grid.Column>
-        <Grid.Column>
+        <Grid.Column className="expert-col">
           <ExpertSection icon="lock" iconClass="lock-align" title="Security" details="Session Storage -- Bcrypt -- Passport.js -- Protected Routes -- Credential Validation with Redux.js" />
         </Grid.Column>
       </Grid.Row>
