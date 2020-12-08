be88dc033bc63d3e5adf4cfaef4095f4307cfbae
ShepLT1 <lshepherd234@gmail.com>
2020-12-08 15:50:45 -0800

    Modified about me paragraphs

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -12,9 +12,12 @@ function AboutMe() {
       </Grid.Row>
       <Grid.Row>
         <Grid.Column id="about-col">
-          <p>I am a full-stack web developer proficient in Javascript-based languages. Prior to my web development education via the University of Washington, I obtained a Bachelor of Science Degree in Psychology with the intention of later applying to a graduate program to become a prosthetics and orthotics clinician. My University of Washington coursework in preparation for a prosthetics career included biology, chemistry, physics, and mathematics.</p>
+          <p>I'm an enthusiastic full-stack web developer proficient in Javascript-based technologies who loves getting down and dirty with some solid code.</p>
 
-          <p>After graduation, I was unfortunately unable to enroll in a prosthetics graduate program due to the development of back issues and the demanding physical nature of a prosthetics career. Since then, I have worked on the administrative side of the healthcare field and am currently employed as a Patient Care Coordinator for a gastroenterology clinic at Swedish Medical Group in Ballard. In my free time, I continue to develop web applications as I am seeking an entry level position as a full-stack engineer.</p>
+
+          <p>Technology has fascinated me ever since I volunteered to set up the family computer as a young kid. My affection for creation also began at a young age, where I spent hours in my room building anything I could imagine out of legos. Web development allows me to combine these interests and I am eager to follow my growing passion.</p>
+
+          <p>I have demonstrated my abilities to work with a team and think critically while building user-friendly web applications. Please have a look at some of my work down below and feel free to check out out my GitHub page, LinkedIn profile, and resume via the links below the navigation menu.</p>
         </Grid.Column>
       </Grid.Row>
     </Grid>
