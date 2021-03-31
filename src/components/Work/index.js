f8a325995f47f944d3316d88ccc384e02edaeee1
ShepLT1 <luke.t.shepherd@gmail.com>
2021-03-30 21:32:54 -0700

    Added best buy bot to projects

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -7,6 +7,7 @@ import PlannerThumbnail from '../../images/day-planner.png'
 import FitnessThumbnail from '../../images/fitness-app.jpeg'
 import WeatherThumbnail from '../../images/weather-forecast.png'
 import SherpaThumbnail from '../../images/5sherpas.png'
+import BuyBotThumbnail from '../../images/bestest-buy-bot.png'
 import Heading from '../Header';
 import "../mainStyle.css";
 import "./style.css"
@@ -23,23 +24,23 @@ function Work() {
           <Project link="https://www.5sherpas.com/" repo="https://github.com/MVC-5/5sherpas" src={SherpaThumbnail} title="5sherpas" brief="The 5sherpas app generates suggestions for constructive activities based on your personal development goals and tracks your weekly progress." />
         </Grid.Column>
         <Grid.Column className="project-col">
-          <Project link="https://breakaway-vacay.herokuapp.com/" repo="https://github.com/MVC-5/breakaway" src={BreakawayThumbnail} title="Breakaway" brief="Breakaway increases productivity by creating a culture of taking breaks. Allows managers to track employee PTO and employees to share breakaways." />
+          <Project link="https://github.com/ShepLT1/bestest-buy-bot/" repo="https://github.com/ShepLT1/bestest-buy-bot/" src={BuyBotThumbnail} title="Best Buy Purchase Bot" brief="Developed to compete with scalpers, the Bestest Buy Bot autonomously purchases high-demand products while utilizing node.js, puppeteer, pm2, and discord web hooks."/>
         </Grid.Column>
       </Grid.Row>
       <Grid.Row columns={2} className="project-row">
+        <Grid.Column className="project-col">
+          <Project link="https://breakaway-vacay.herokuapp.com/" repo="https://github.com/MVC-5/breakaway" src={BreakawayThumbnail} title="Breakaway" brief="Breakaway increases productivity by creating a culture of taking breaks. Allows managers to track employee PTO and employees to share breakaways." />
+        </Grid.Column>
         <Grid.Column className="project-col">
           <Project link="https://sheplt1.github.io/Day-Planner/" repo="https://github.com/ShepLT1/Day-Planner" src={PlannerThumbnail} title="Day Planner" brief="Day Planner allows the user to save events for each work hour of a selected workday. Events are color-coded based on past, current, or upcoming." />
         </Grid.Column>
-        <Grid.Column className="project-col">
-          <Project link="https://ls-fitness-witness.herokuapp.com/?id=5f938a806451f30017fe87c5" repo="https://github.com/ShepLT1/fitness-witness" src={FitnessThumbnail} title="Fitness Witness" brief="Fitness Witness is a fitness tracker application that allows user to add exercises to a previous or new workout and view stats for multiple workouts." />
-        </Grid.Column>
       </Grid.Row>
       <Grid.Row columns={2} className="project-row">
         <Grid.Column className="project-col">
-          <Project link="https://sheplt1.github.io/hows-the-weather/" repo="https://github.com/ShepLT1/hows-the-weather" src={WeatherThumbnail} title="How's the Weather" brief="How's the Weather utilizes search functionality to find current and projected weather conditions for user-selected cities." />
+          <Project link="https://ls-fitness-witness.herokuapp.com/?id=5f938a806451f30017fe87c5" repo="https://github.com/ShepLT1/fitness-witness" src={FitnessThumbnail} title="Fitness Witness" brief="Fitness Witness is a fitness tracker application that allows user to add exercises to a previous or new workout and view stats for multiple workouts." />
         </Grid.Column>
         <Grid.Column className="project-col">
-          <Project link="https://sheplt1.github.io/Code-Quiz/" repo="https://github.com/ShepLT1/Code-Quiz/" src={QuizThumbnail} title="Code Quiz" brief="Code Quiz tests the user's Javascript knowledge and allows for saving and viewing of highscores." />
+          <Project link="https://sheplt1.github.io/hows-the-weather/" repo="https://github.com/ShepLT1/hows-the-weather" src={WeatherThumbnail} title="How's the Weather" brief="How's the Weather utilizes search functionality to find current and projected weather conditions for user-selected cities." />
         </Grid.Column>
       </Grid.Row>
     </Grid>
