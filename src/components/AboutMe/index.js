069af2a37cffdc6255a4661f3598b602b7c6ff18
ShepLT1 <lshepherd234@gmail.com>
2022-11-18 19:11:05 -0800

    Added webpack css preloader to handle compilation errors

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -14,9 +14,9 @@ function AboutMe() {
         <Grid.Column id="about-col">
           <p>I'm an enthusiastic full-stack software engineer specializing in Javascript-based frameworks who loves building robust, scalable applications.</p>
 
-          <p>I'm a self-starter who channeled childhood interests of computer hardware and lego into a career as a full-stack engineer. I have taken on many roles while working at a tech startup, including both front and back end engineer, architecture lead, product manager, external systems integration specialist, de-facto dev-ops, and countless others. My professional engineering experience within the tech startup world has helped hone my technical abilities while teaching me to be agile and decisive when faced with daily challenges. Adopting a growth-oriented mindset and consistently fueling my curiosity engine have helped build a great start to my career, and I'm excited to see what the future holds.</p>
+          <p>As a self-starter, I took on many roles while working at a tech startup, including both front and back end engineer, architecture lead, product manager, external systems integration specialist, de-facto dev-ops, and countless others. My professional engineering experience within the tech startup world has helped hone my technical abilities while teaching me to be agile and decisive when faced with daily challenges. Adopting a growth-oriented mindset and consistently fueling my curiosity engine have helped build a great start to my career, and I'm excited to see what the future holds.</p>
 
-          <p>I thoroughly enjoy working with a team of engineers to build efficient, large-scale, and user-friendly web applications. Please have a look at some of my work down below and feel free to check out out my GitHub page, LinkedIn profile, and resume via the links below the navigation menu.</p>
+          <p>I thoroughly enjoy working with a team of engineers to build efficient, large-scale, and user-friendly applications. Please have a look at some of my work down below and feel free to check out out my GitHub page, LinkedIn profile, and resume via the links below the navigation menu.</p>
         </Grid.Column>
       </Grid.Row>
     </Grid>
