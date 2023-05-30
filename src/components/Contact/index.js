d38de0c6643da49a8dd0590facfdd9a19e2dcfe6
ShepLT1 <luke.t.shepherd@gmail.com>
2023-05-29 18:09:20 -0700

    Update phone number spacing

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Heading from '../Header';
-import { Grid } from 'semantic-ui-react';
-import './style.css';
+import Heading from "../Header";
+import { Grid } from "semantic-ui-react";
+import "./style.css";
 
 function Contact() {
-
   return (
     <Grid stackable centered id="contact">
       <Grid.Row id="contact-title-container">
@@ -12,13 +11,22 @@ function Contact() {
       </Grid.Row>
       <Grid.Row>
         <Grid.Column id="contact-col">
-          <p>I am currently located in Seattle, Washington. Please feel free to reach out to me.</p>
-          <a href="mailto:luke.t.shepherd@gmail.com" className="contact-info" id="email-link">luke.t.shepherd@gmail.com</a>
-          <p className="contact-info">(425)736-6565</p>
+          <p>
+            I am currently located in Seattle, Washington. Please feel free to
+            reach out to me.
+          </p>
+          <a
+            href="mailto:luke.t.shepherd@gmail.com"
+            className="contact-info"
+            id="email-link"
+          >
+            luke.t.shepherd@gmail.com
+          </a>
+          <p className="contact-info">(425) 736-6565</p>
         </Grid.Column>
       </Grid.Row>
     </Grid>
   );
-};
+}
 
 export default Contact;
