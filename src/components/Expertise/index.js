091657bc3990bc366ead9926e432bfe35facbf78
ShepLT1 <luke.t.shepherd@gmail.com>
2023-05-29 17:05:46 -0700

    Update expertise section

--- src/components/Expertise/index.js
+++ src/components/Expertise/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Grid } from 'semantic-ui-react';
-import Heading from '../../components/Header';
-import ExpertSection from '../../components/ExpertSection';
-import './styles.css';
+import { Grid } from "semantic-ui-react";
+import Heading from "../../components/Header";
+import ExpertSection from "../../components/ExpertSection";
+import "./styles.css";
 
 function Expertise() {
-
   return (
     <Grid stackable centered id="expertise">
       <Grid.Row>
@@ -13,18 +12,37 @@ function Expertise() {
       </Grid.Row>
       <Grid.Row columns={2} className="expert-row">
         <Grid.Column className="expert-col">
-          <ExpertSection icon="desktop" title="Front End" details="React -- React Router -- Redux -- HTML -- CSS -- Axios -- Mobile Responsive" />
+          <ExpertSection
+            icon="desktop"
+            title="Front End"
+            details="React -- React Router -- Redux -- HTML -- CSS -- Axios -- Mobile Responsive"
+          />
         </Grid.Column>
         <Grid.Column className="expert-col">
-          <ExpertSection icon="server" iconClass="server-align" title="Back End" details="Node -- Express -- REST API -- Mongoose -- User/Route Authentication -- Webhooks -- JWT" />
+          <ExpertSection
+            icon="server"
+            iconClass="server-align"
+            title="Back End"
+            details="Node -- Express -- Mongoose -- Django -- Django REST Framework -- REST/Bulk API -- User/Route Authentication -- Webhooks -- JWT"
+          />
         </Grid.Column>
       </Grid.Row>
       <Grid.Row columns={2} className="expert-row">
         <Grid.Column className="expert-col">
-          <ExpertSection icon="database" iconClass="db-align" title="Data Management" details="MongoDB -- Mongoose -- NoSQL -- SQL -- MySQL -- Scalable Database Architecture" />
+          <ExpertSection
+            icon="database"
+            iconClass="db-align"
+            title="Data Management"
+            details="MongoDB -- Mongoose -- NoSQL -- SQL -- Postgres -- MySQL -- Scalable Database Architecture"
+          />
         </Grid.Column>
         <Grid.Column className="expert-col">
-          <ExpertSection icon="cog" iconClass="cog-align" title="General" details="MERN -- SERN -- Javascript -- Python -- OOP -- MVC Paradigm -- Git -- Heroku -- Computer Science -- Data Structures" />
+          <ExpertSection
+            icon="cog"
+            iconClass="cog-align"
+            title="General"
+            details="MERN -- SERN -- Javascript -- Python -- AWS -- Heroku -- Docker -- Terraform -- Git -- CLI -- OOP -- MVC Paradigm -- Computer Science -- Data Structures -- Integrations"
+          />
         </Grid.Column>
       </Grid.Row>
     </Grid>
